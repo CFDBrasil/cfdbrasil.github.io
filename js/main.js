443ef359bc574d999a4b9bf8e66ea1bbe8861309
Rafael Cabral de Moura <rafael.xuxubelez@gmail.com>
2017-02-02 23:28:42 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+// grab an element
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init(); 
+*/
